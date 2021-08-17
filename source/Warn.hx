3b7345d7fb916dc53641bf964c60333c0c865dde
Chris <62375193+ChrisLad1@users.noreply.github.com>
2021-08-18 07:42:04 +1000

    s

---
+++ source/Warn.hx
@@ -0,0 +1,54 @@
+package;
+
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.graphics.FlxGraphic;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRect;
+import flixel.system.FlxSound;
+import flixel.system.ui.FlxSoundTray;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import io.newgrounds.NG;
+import lime.app.Application;
+import openfl.Assets;
+
+
+#if windows
+import Discord.DiscordClient;
+#end
+
+#if cpp
+import sys.thread.Thread;
+#end
+
+using StringTools;
+
+class warn
+    {
+        function warning()
+            {
+
+                warning = new FlxSprite();
+                warning.frames = Paths.getSparrowAtlas('MenuWarn');
+                warning.animation.addByPrefix('idle', 'MenuWarning', 24);
+                warning.antialiasing = true;
+                warning.animation.play('idle');
+                add(warning);
+
+            }
+        
+    }
